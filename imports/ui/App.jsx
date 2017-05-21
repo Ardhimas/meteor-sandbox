8b5620039e83ad6d0d728b936c0a991046fc02f8
Ardy Kamdani <kamdani.af@gmail.com>
2017-05-21 17:00:12 -0500

    refactored friends panel into its own component as well

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,66 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import { Card, Button, Row, Col } from 'react-materialize';
-
-import Users from '../api/users';
+import { Row, Col } from 'react-materialize';
 
 import MessagesPanel from './MessagesPanel.jsx';
-import User from './User.jsx';
-
-import { filterFriends, filterNotFriends } from '../utils.js';
+import FriendsPanel from './FriendsPanel.jsx';
 
 // App component - represents the whole app
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      hideCompleted: false,
       receiver: null,
-      selectedTab: 'Friends',
     };
-
-    this.handleChangeTab = this.handleChangeTab.bind(this);
-  }
-
-  handleChangeTab(newTab) {
-    this.setState({ selectedTab: newTab });
-  }
-
-  renderUsers() {
-    const { allUsers, currentUser } = this.props;
-    if (currentUser) {
-      if (this.state.selectedTab === 'Friends') {
-        const friendUsers = filterFriends(currentUser.friendIds, allUsers);
-        return friendUsers.map(user => (
-          <User
-            key={user._id}
-            user={user}
-            setReceiverId={() => this.setState({ receiver: user })}
-            isFriend
-          />
-        ));
-      } else if (this.state.selectedTab === 'Add Friends') {
-        const friendUsers = filterNotFriends(currentUser.friendIds, allUsers);
-        return friendUsers.map(user => (
-          <User
-            key={user._id}
-            user={user}
-            setReceiverId={() => this.setState({ receiver: user })}
-            isFriend={false}
-          />
-        ));
-      }
-    }
-    return <noscript />;
   }
 
   render() {
     const { currentUser } = this.props;
     const isChatting = !!this.state.receiver;
     const { _id, username } = isChatting && this.state.receiver;
-    const isAddingFriends = this.state.selectedTab === 'Add Friends';
     return (
       <Row className="container">
         <div id="stars" />
@@ -75,24 +33,9 @@ class App extends Component {
           />
         </Col>
         <Col s={12} m={4}>
-          { currentUser &&
-            <Card id="" className="panel">
-              <header>
-                <h1>{this.state.selectedTab}</h1>
-                <Button
-                  floating
-                  className="btn-navigation"
-                  icon={isAddingFriends ? 'chevron_left' : 'add'}
-                  onClick={() => this.setState({ selectedTab: (isAddingFriends ? 'Friends' : 'Add Friends') })}
-                >
-                  Add Friends
-                </Button>
-              </header>
-              <ul className="list">
-                {this.renderUsers()}
-              </ul>
-            </Card>
-          }
+          <FriendsPanel
+            setReceiver={receiver => this.setState({ receiver })}
+          />
         </Col>
         <footer>Made with ❤ by Ardhimas</footer>
       </Row>
@@ -106,13 +49,11 @@ App.defaultProps = {
 
 App.propTypes = {
   currentUser: PropTypes.object,
-  allUsers: PropTypes.array.isRequired,
 };
 
 export default createContainer(() => {
   Meteor.subscribe('users');
   return {
     currentUser: Meteor.user(),
-    allUsers: Users.find({}).fetch(),
   };
 }, App);
---
+++ imports/ui/FriendsPanel.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { Card, Button } from 'react-materialize';
+
+import Users from '../api/users';
+
+import User from './User.jsx';
+
+import { filterFriends, filterNotFriends } from '../utils.js';
+
+// App component - represents the whole app
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'Friends',
+    };
+  }
+
+  renderUsers() {
+    const { allUsers, currentUser } = this.props;
+    if (currentUser) {
+      if (this.state.selectedTab === 'Friends') {
+        const friendUsers = filterFriends(currentUser.friendIds, allUsers);
+        return friendUsers.map(user => (
+          <User
+            key={user._id}
+            user={user}
+            setReceiverId={() => this.props.setReceiver(user)}
+            isFriend
+          />
+        ));
+      } else if (this.state.selectedTab === 'Add Friends') {
+        const friendUsers = filterNotFriends(currentUser.friendIds, allUsers);
+        return friendUsers.map(user => (
+          <User
+            key={user._id}
+            user={user}
+            setReceiverId={() => this.props.setReceiver(user)}
+            isFriend={false}
+          />
+        ));
+      }
+    }
+    return <noscript />;
+  }
+
+  render() {
+    const { currentUser } = this.props;
+    const { selectedTab } = this.state;
+    const isAddingFriends = selectedTab === 'Add Friends';
+    return (
+      currentUser &&
+        <Card id="" className="panel">
+          <header>
+            <h1>{selectedTab}</h1>
+            <Button
+              floating
+              className="btn-navigation"
+              icon={isAddingFriends ? 'chevron_left' : 'add'}
+              onClick={() => this.setState({ selectedTab: (isAddingFriends ? 'Friends' : 'Add Friends') })}
+            />
+          </header>
+          <ul className="list">
+            {this.renderUsers()}
+          </ul>
+        </Card>
+    );
+  }
+}
+
+App.defaultProps = {
+  currentUser: null,
+};
+
+App.propTypes = {
+  currentUser: PropTypes.object,
+  allUsers: PropTypes.array.isRequired,
+  setReceiver: PropTypes.func.isRequired,
+};
+
+export default createContainer(() => {
+  Meteor.subscribe('users');
+  return {
+    currentUser: Meteor.user(),
+    allUsers: Users.find({}).fetch(),
+  };
+}, App);
